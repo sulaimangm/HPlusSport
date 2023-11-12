64b39f28655a8936b7ed33e2f0715bf14e1d12a1
sulaimangm <sulaimangm@gmail.com>
2023-11-12 16:46:53 -0500

    IMplementing Update and Delete

--- HPlusSport.API/Controllers/ProductsController.cs
+++ HPlusSport.API/Controllers/ProductsController.cs
@@ -42,5 +42,41 @@ namespace HPlusSport.API.Controllers
 
             return CreatedAtAction("GetProduct", new { id = product.Id }, product);
         }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> PutProduct(int id, Product product)
+        {
+            if (id != product.Id)
+                return BadRequest();
+
+            _context.Entry(product).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch(DbUpdateConcurrencyException)
+            {
+                if (!_context.Products.Any(p => p.Id == id))
+                    return NotFound();
+                else
+                    throw;
+            }
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Product>> DeleteProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if(product == null)
+                return NotFound();
+            
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+
+            return product;
+
+        }
     }
 }
--- HPlusSport.API/Program.cs
+++ HPlusSport.API/Program.cs
@@ -6,10 +6,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers()
-    .ConfigureApiBehaviorOptions(options => 
-    {
-        options.SuppressModelStateInvalidFilter = true;
-    });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
