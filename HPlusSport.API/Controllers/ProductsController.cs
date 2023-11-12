518e38fddc9e384bb66fc42590898aea08c783d5
sulaimangm <sulaimangm@gmail.com>
2023-11-12 16:26:19 -0500

    Add Model Validation

--- HPlusSport.API/Controllers/ProductsController.cs
+++ HPlusSport.API/Controllers/ProductsController.cs
@@ -21,12 +21,26 @@ namespace HPlusSport.API.Controllers
         {
             return Ok(await _context.Products.ToArrayAsync());
         }
+
         [HttpGet]
         [Route("{id}")]
         public async Task<ActionResult> GetProduct(int id)
         {
             var product = await _context.Products.FirstOrDefaultAsync(x => x.Id == id);
             return product == null ? NotFound() : Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Product>> PostProduct(Product product)
+        {
+            /*if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }*/
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProduct", new { id = product.Id }, product);
         }
     }
 }
