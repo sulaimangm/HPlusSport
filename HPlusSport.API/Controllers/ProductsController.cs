52864fcf2c355b65768a946d339d8fb133008b86
sulaimangm <sulaimangm@gmail.com>
2023-11-11 19:18:43 -0500

    Add project files.

---
+++ HPlusSport.API/Controllers/ProductsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HPlusSport.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : Controller
+    {
+        [HttpGet]
+        public string GetProducts()
+        {
+            return "OK";
+        }
+    }
+}
